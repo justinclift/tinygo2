3dc499640838a0b0737df7c4d01406e8605addd3
Justin Clift <justin@postgresql.org>
2019-05-17 23:41:53 +1000

    Not quite right yet.  This still pulls in syscall/js

--- main.go
+++ main.go
@@ -6,13 +6,11 @@ import (
 
 func main () {
 	t := time.Now()
-	print("Start time: ", t.Hour(), ":", t.Minute(), ":", t.Second(), "\n")
 	for i := 0; i < 500000; i++ {
 		t = time.Now()
 		wasteRam(t)
 	}
 	t = time.Now()
-	print("End time: ", t.Hour(), ":", t.Minute(), ":", t.Second(), "\n")
 }
 
 func wasteRam(t time.Time) {
@@ -24,9 +22,6 @@ func wasteRam(t time.Time) {
 	f := 0
 	if (e % 7) == 0 {
 		f++
-	}
-	if f > 10000 {
-		println("f has reached 10k")
 	}
 }
 
